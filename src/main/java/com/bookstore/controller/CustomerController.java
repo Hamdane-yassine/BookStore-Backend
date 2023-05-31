cb0c843f3f4d1677f24e7d201f5eb1a14db9f3f0
Yassine Hamdane <hamdaneyassine10@gmail.com>
2023-05-31 22:13:26 +0100

    modify get endpoint

--- src/main/java/com/bookstore/controller/CustomerController.java
+++ src/main/java/com/bookstore/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.bookstore.secured.service.CustomerServiceSecured;
 import com.bookstore.unsecured.model.Customer;
 import com.bookstore.unsecured.service.CustomerServiceUnsecured;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,8 +28,8 @@ public class CustomerController {
     }
 
     @GetMapping("/unsecured/borrows")
-    public List<Customer> getCustomerBorrowsByEmailUnsecured(@RequestBody com.bookstore.unsecured.dto.BorrowsRequest borrowsRequest) {
-        return customerServiceUnsecured.getCustomerBorrowsByEmail(borrowsRequest.getEmail());
+    public List<Customer> getCustomerBorrowsByEmailUnsecured(@RequestParam String email) {
+        return customerServiceUnsecured.getCustomerBorrowsByEmail(email);
     }
 
     @PostMapping("/secured")
@@ -37,8 +38,8 @@ public class CustomerController {
     }
 
     @GetMapping("/secured/borrows")
-    public List<CustomerSecured> getCustomerBorrowsByEmailSecured(@RequestBody com.bookstore.secured.dto.BorrowsRequest borrowsRequest) {
-        return customerServiceSecured.getCustomerBorrowsByEmail(borrowsRequest.getEmail());
+    public List<CustomerSecured> getCustomerBorrowsByEmailSecured(@Email @RequestParam String email) {
+        return customerServiceSecured.getCustomerBorrowsByEmail(email);
     }
 }
 
